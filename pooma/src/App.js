a96aa064c504042191217dcb80c803b95710b1c8
unknown <bubbastinx@gmail.com>
2018-10-03 10:47:43 -0500

    added Links via react-router-dom

--- pooma/src/App.js
+++ pooma/src/App.js
@@ -1,24 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import NavComponent from './components/Layout/Navbar.js'
+import NavComponent from './components/Layout/Navbar.js';
 import './App.css';
-import Discussion from './components/Layout/Discussion'
-import Footer from './components/Layout/Footer.js'
-import Sidebar from './components/Layout/Sidebar.js'
-
+import Discussion from './components/Layout/Discussion';
+import Footer from './components/Layout/Footer.js';
+import Sidebar from './components/Layout/Sidebar.js';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          <NavComponent /> 
-          <div className="container">
-          <Discussion />
-          <Sidebar />
+      <Router>
+        <div className="App">
+          <NavComponent />
+
+          <div className="container ">
+            <Discussion />
+            <Sidebar />
           </div>
-      
-         <Footer />
-      </div>
+
+          <Footer />
+        </div>
+      </Router>
     );
   }
 }
--- pooma/src/components/Layout/Navbar.js
+++ pooma/src/components/Layout/Navbar.js
@@ -1,44 +1,45 @@
 import React from 'react';
-// import ReactDOM from 'react-dom';
- 
+import { Link } from 'react-router-dom';
 
-export default class NavComponent extends React.Component {
-    render() {
-        return (
-            <nav>
-                <div className="navbar navWide">
-                    <div className="navbar-brand">
-                       <h2>Pooma</h2> 
-                    </div>
-                    <div className="wideDiv">
-                        <a href="#">Link 1</a>
-                        <a href="#">Link 2</a>
-                        <a href="#">Link 3</a>
-                    </div>
-                </div>
-                <div className="navbar navNarrow">
-                    <i className="fa fa-bars fa-2x" onClick={this.hamburgerToggle}></i>
-                    <div className="narrowLinks" ref="narrowLinks">
-                        <a href="#">Link 1</a>
-                        <a href="#">Link 2</a>
-                        <a href="#">Link 3</a>
-                    </div>
-                </div>
-            </nav>
-        );
-    }
+class NavComponent extends React.Component {
+  render() {
+    return (
+      <nav>
+        <div className="navbar navWide">
+          <div className="navbar-brand">
+            <h2>Pooma</h2>
+          </div>
+          <div className="wideDiv">
+            <Link to="#">Link 1</Link>
+            <Link to="#">Link 2</Link>
+            <Link to="#">Link 3</Link>
+          </div>
+        </div>
+        <div className="navbar navNarrow">
+          <i className="fa fa-bars fa-2x" onClick={this.hamburgerToggle} />
+          <div className="narrowLinks" ref="narrowLinks">
+            <Link to="/About">Link 1</Link>
+            <Link to="/OtherStuff">Link 2</Link>
+            <Link to="/Test">Link 3</Link>
+          </div>
+        </div>
+      </nav>
+    );
+  }
 
-    hamburgerToggle = () => {
-        let links = this.refs.narrowLinks;
-        console.log(links);
-        
-        if (links.style.display === 'block') {
-            links.style.display = 'none';
-        } else {
-            links.style.display = 'block';
-        }
+  hamburgerToggle = () => {
+    let links = this.refs.narrowLinks;
+    console.log(links);
+
+    if (links.style.display === 'block') {
+      links.style.display = 'none';
+    } else {
+      links.style.display = 'block';
     }
-};
+  };
+}
+
+export default NavComponent;
 
 // ReactDOM.render(<NavComponent />, document.getElementById('navbar'))
 // registerServiceWorker();
