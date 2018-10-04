c03bc5776f40bb11ce40d56a4bde96e7fe5a0f8a
unknown <bubbastinx@gmail.com>
2018-10-04 13:25:19 -0500

    changed folder structure to match to best practices

---
+++ pooma/src/components/Layout/Sidebar/Sidebar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Sidebar extends Component {
+  render() {
+    return (
+      <sidebar>
+        <h2>This will be the Sidebar Component</h2>
+      </sidebar>
+    );
+  }
+}
+
+export default Sidebar;
