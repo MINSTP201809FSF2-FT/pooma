95a73188ec7fc0188026fd1c231e1ec9156e3618
unknown <bubbastinx@gmail.com>
2018-10-08 15:00:15 -0500

    changed server port

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3666;
 
 function errorHandler(err, req, res, next) {
   if (res.headersSent) {
